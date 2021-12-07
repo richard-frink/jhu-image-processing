5f901dc309476c5728c6932b1608593cdd5d27d2
Ricky <rsussan12@gmail.com>
2021-12-06 21:14:32 -0500

    trying to use wavelet-monodepth

---
+++ project/plane_creator.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import cv2 as cv
+import numpy as np
+
+class PlaneCreator:
+    def find_planes(self, image):
+        return []
---
+++ project/program.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import cv2 as cv
+import numpy as np
+from plane_creator import PlaneCreator
+from depth_creator import DepthCreator
+from video_interpreter import VideoInterpreter
+
+
+def main():
+    print("asdlkfjhn")
+
+
+main()
