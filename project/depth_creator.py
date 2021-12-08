fda4c78e7e09360985c80535a279a846ba48fb65
Ricky <rsussan12@gmail.com>
2021-12-07 22:21:56 -0500

    final version of video interpretor with evaluated videos

--- project/depth_creator.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import cv2 as cv
-import numpy as np
-
-class DepthCreator:
-    def create_depth_image(self, image):
-        return []
--- project/video_interpreter.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import cv2 as cv
-import numpy as np
-
-class VideoInterpreter:
-    def read_video(self, file_path):
-        return []
